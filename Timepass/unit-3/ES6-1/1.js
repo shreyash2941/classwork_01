ab40849860a227227f000b33bff90aa49408b73b
Shreyash <birajdarshreyash@gmail.com>
2022-12-17 22:32:11 +0530

    added

---
+++ Timepass/unit-3/ES6-1/1.js
@@ -0,0 +1,9 @@
+let API_KEY = 'AIzaSyD0EhBYE54ml54mSgPPPBTvCsM7O78-DrA';
+let container = document.getElementById("container");
+
+async function getData(){
+    let query = document.getElementById("query");
+
+    
+
+}
