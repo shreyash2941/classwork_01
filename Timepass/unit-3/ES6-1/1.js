6c47566cf5ffdfddfc993469e8f5a01c3a1719e1
Shreyash <birajdarshreyash@gmail.com>
2022-12-17 22:52:35 +0530

    added

--- Timepass/unit-3/ES6-1/1.js
+++ Timepass/unit-3/ES6-1/1.js
@@ -2,8 +2,22 @@ let API_KEY = 'AIzaSyD0EhBYE54ml54mSgPPPBTvCsM7O78-DrA';
 let container = document.getElementById("container");
 
 async function getData(){
-    let query = document.getElementById("query");
+    try {
+        let query = document.getElementById("query");
 
-    
+        let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${query}&key=${API_KEY}&PART=snippet&maxResults=20`);
+        let {items} = await res.json();
+        let data_array = items;
+        console.log(data_array)
+    } catch (error) {
+        console.log(error);
+    }
 
+}
+
+
+function showData(){
+    data.forEach(function(el){
+        
+    })
 }
